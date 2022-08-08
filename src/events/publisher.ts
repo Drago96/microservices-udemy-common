7fafda020a98dc44e4589bd3a7967d40a33e2d7f
dproychev <dproychev@teladoc.com>
2022-08-09 01:51:59 +0300

    Updates

--- src/events/publisher.ts
+++ src/events/publisher.ts
@@ -11,8 +11,8 @@ export abstract class Publisher<T extends Event> {
 
   constructor(private client: Stan) {}
 
-  publish(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
+  publish(data: T["data"]) {
+    return new Promise<void>((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (error) => {
         if (error) {
           return reject(error);
