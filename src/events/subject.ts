340f942b8880cc689c55d56f048e8faac0a44a38
dproychev <dproychev@teladoc.com>
2022-08-12 03:17:39 +0300

    Updates

--- src/events/subject.ts
+++ src/events/subject.ts
@@ -1,6 +1,7 @@
 export enum Subject {
   TicketCreated = "ticket:created",
   TicketUpdated = "ticket:updated",
+  TicketNotReserved = "ticket:not-reserved",
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
   ExpirationComplete = "expiration:complete",
